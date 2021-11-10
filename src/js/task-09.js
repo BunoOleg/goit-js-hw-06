c3b04e51c8ac2452c2fbb0426f73d7f6132eb80a
BunoOleg <86609721+BunoOleg@users.noreply.github.com>
2021-11-10 21:11:44 +0200

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,7 +1,7 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-const bodyEl = document.body;
+const bodyEl = document.querySelector("body");
 const buttonEl = document.querySelector(".change-color");
 const spanEl = document.querySelector(".color");
 
